7c57464e84bd889745a3ac9ab700d0a8abac5d5f
Zhiqing Qiu <zhqqi@microsoft.com>
2019-03-15 16:25:55 +0800

    2

--- app/scripts/modules/azure/utility.ts
+++ app/scripts/modules/azure/utility.ts
@@ -1,4 +1,4 @@
-interface Tag {
+interface ITag {
   key: string;
   value: string;
 }
@@ -9,18 +9,28 @@ export default class Utility {
   public static readonly TAG_VALUE_LENGTH_LIMITATION: number = 6;
   public static readonly TAG_INVALID_CHAR_REG_EXR: RegExp = /[<>%&\\?/]/g;
 
-  public static getTagErrorMessage(tagsObject: Tag): string | null {
-    if (!tagsObject) return 'instanceTags is not defined';
-    let length: number = Object.keys(tagsObject).length;
-    if (!(length >= 0 && length <= Utility.TAG_LIMITATION))
+  public static getTagErrorMessage(tagsObject: ITag): string | null {
+    if (!tagsObject) {
+      return 'instanceTags is not defined';
+    }
+    const length: number = Object.keys(tagsObject).length;
+    if (!(length >= 0 && length <= Utility.TAG_LIMITATION)) {
       return `Number of tags exceeds the limit: ${Utility.TAG_LIMITATION}`;
-    for (let [k, v] of Object.entries(tagsObject)) {
-      if (k.length > Utility.TAG_KEY_LENGTH_LIMITATION)
+    }
+
+    for (const [k, v] of Object.entries(tagsObject)) {
+      if (k.length > Utility.TAG_KEY_LENGTH_LIMITATION) {
         return `Length of Tag key: ${k} exceeds the limit: ${Utility.TAG_KEY_LENGTH_LIMITATION}`;
-      if (v.length > Utility.TAG_VALUE_LENGTH_LIMITATION)
-        `Length of Tag value: ${v} exceeds the limit: ${Utility.TAG_VALUE_LENGTH_LIMITATION}`;
-      if (Utility.TAG_INVALID_CHAR_REG_EXR.test(k)) return `Invalid characters in Tag key: ${k}`;
-      if (Utility.TAG_INVALID_CHAR_REG_EXR.test(v)) `Invalid characters in Tag value: ${v}`;
+      }
+      if (v.length > Utility.TAG_VALUE_LENGTH_LIMITATION) {
+        return `Length of Tag value: ${v} exceeds the limit: ${Utility.TAG_VALUE_LENGTH_LIMITATION}`;
+      }
+      if (Utility.TAG_INVALID_CHAR_REG_EXR.test(k)) {
+        return `Invalid characters in Tag key: ${k}`;
+      }
+      if (Utility.TAG_INVALID_CHAR_REG_EXR.test(v)) {
+        return `Invalid characters in Tag value: ${v}`;
+      }
     }
     return null;
   }
